dc9fe212ae40c1f9b449017588a926c92bb99715
Fábio Matias <fabio_mcm2@hotmail.com>
2023-02-07 21:56:57 +0000

    List bars for a city

--- app/page.tsx
+++ app/page.tsx
@@ -1,152 +1,145 @@
-'use client'
+"use client";
 
-import React, { useState, useEffect } from 'react'
-import ReactMarkdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
+import React, { useState, useEffect } from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 export default function Home() {
-  const [request, setRequest] = useState<{days?: string, city?: string}>({})
-  let [itinerary, setItinerary] = useState<string>('')
+  const [request, setRequest] = useState<{ city?: string }>({});
+  let [itinerary, setItinerary] = useState<string>("");
 
-  useEffect(() => {
-    checkRedirect()
-  }, [])
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState("");
 
-  function checkRedirect() {
-    if (window.location.hostname === 'gpt-travel-advisor.vercel.app') {
-      window.location.replace('https://www.roamaround.io/')
-    }
-  }
-
-  const [loading, setLoading] = useState(false)
-  const [message, setMessage] = useState('')
   async function hitAPI() {
-    if (!request.city || !request.days) return
-    setMessage('Building itinerary...')
-    setLoading(true)
-    setItinerary('')
+    if (!request.city) return;
+    setMessage("Building itinerary...");
+    setLoading(true);
+    setItinerary("");
 
     setTimeout(() => {
-      setMessage('Getting closer ...')
-    }, 7000)
+      setMessage("Getting closer ...");
+    }, 7000);
 
     setTimeout(() => {
-      setMessage('Almost there ...')
-    }, 15000)
+      setMessage("Almost there ...");
+    }, 15000);
 
-    const response = await fetch('/api/get-itinerary', {
-      method: 'POST',
+    const response = await fetch("/api/get-itinerary", {
+      method: "POST",
       body: JSON.stringify({
-        days: request.days,
-        city: request.city
-      })
-    })
-    const json = await response.json()
-    
-    const response2 = await fetch('/api/get-points-of-interest', {
-      method: 'POST',
+        city: request.city,
+      }),
+    });
+
+    const json = await response.json();
+
+    const response2 = await fetch("/api/get-points-of-interest", {
+      method: "POST",
       body: JSON.stringify({
         pointsOfInterestPrompt: json.pointsOfInterestPrompt,
-      })
-    })
-    const json2 = await response2.json()
+      }),
+    });
 
-    let pointsOfInterest = JSON.parse(json2.pointsOfInterest)
-    let itinerary = json.itinerary
+    const json2 = await response2.json();
 
-    pointsOfInterest.map(point => {
-      // itinerary = itinerary.replace(point, `<a target="_blank" rel="no-opener" href="https://www.google.com/search?q=${encodeURIComponent(point + ' ' + request.city)}">${point}</a>`)
-      itinerary = itinerary.replace(point, `[${point}](https://www.google.com/search?q=${encodeURIComponent(point + ' ' + request.city)})`)
-    })
+    let pointsOfInterest = JSON.parse(json2.pointsOfInterest);
+    let itinerary = json.itinerary;
 
-    setItinerary(itinerary)
-    setLoading(false)
-  }
-  
-  let days = itinerary.split('Day')
+    pointsOfInterest.map((point) => {
+      itinerary = itinerary.replace(
+        point,
+        `[${point}](https://www.google.com/search?q=${encodeURIComponent(
+          point + " " + request.city
+        )})`
+      );
+    });
 
-  if (days.length > 1) {
-    days.shift()
-  } else {
-    days[0] = "1" + days[0]
+    setItinerary(itinerary);
+    setLoading(false);
   }
 
   return (
     <main>
       <div className="app-container">
-        <h1 style={styles.header} className="hero-header">Roam Around</h1>
-        <div style={styles.formContainer} className="form-container">
-          <input style={styles.input}  placeholder="City" onChange={e => setRequest(request => ({
-            ...request, city: e.target.value
-          }))} />
-          <input style={styles.input} placeholder="Days" onChange={e => setRequest(request => ({
-            ...request, days: e.target.value
-          }))} />
-          <button className="input-button"  onClick={hitAPI}>Build Itinerary</button>
-        </div>
+        <h1 style={styles.header} className="hero-header">
+          Bar Crawl
+        </h1>
+        <form
+          style={styles.formContainer}
+          className="form-container"
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <input
+            style={styles.input}
+            placeholder="City"
+            onChange={(e) =>
+              setRequest((request) => ({
+                ...request,
+                city: e.target.value,
+              }))
+            }
+          />
+          <button type="submit" className="input-button" onClick={hitAPI}>
+            Build Itinerary
+          </button>
+        </form>
         <div className="results-container">
-        {
-          loading && (
-            <p>{message}</p>
-          )
-        }
-        {
-          itinerary && days.map((day, index) => (
-            // <p
-            //   key={index}
-            //   style={{marginBottom: '20px'}}
-            //   dangerouslySetInnerHTML={{__html: `Day ${day}`}}
-            // />
-            <div
-              style={{marginBottom: '30px'}}
-              key={index}
-            >
-              <ReactMarkdown
-              remarkPlugins={[remarkGfm]}
-              components={{
-                a: props => {
-                    return <a target="_blank" rel="no-opener" href={props.href}>{props.children}</a>
-                }
-            }}
-              >
-                {`Day ${day}`}
-                </ReactMarkdown>
-            </div>
-          ))
-        }
-
+          {loading && <p>{message}</p>}
+          {itinerary &&
+            itinerary
+              .split("\n")
+              .filter((bar) => !!bar)
+              .map((bar, index) => (
+                <div style={{ marginBottom: "30px" }}>
+                  <ReactMarkdown
+                    remarkPlugins={[remarkGfm]}
+                    components={{
+                      a: (props) => {
+                        return (
+                          <a target="_blank" rel="no-opener" href={props.href}>
+                            {props.children}
+                          </a>
+                        );
+                      },
+                    }}
+                  >
+                    {`${index + 1}. ${bar}`}
+                  </ReactMarkdown>
+                </div>
+              ))}
         </div>
       </div>
     </main>
-  )
+  );
 }
 
 const styles = {
   header: {
-    textAlign: 'center' as 'center',
-    marginTop: '60px',
-    color: '#c683ff',
-    fontWeight: '900',
-    fontFamily: 'Poppins',
-    fontSize: '68px'
+    textAlign: "center" as "center",
+    marginTop: "60px",
+    color: "#c683ff",
+    fontWeight: "900",
+    fontFamily: "Poppins",
+    fontSize: "68px",
   },
   input: {
-    padding: '10px 14px',
-    marginBottom: '4px',
-    outline: 'none',
-    fontSize: '16px',
-    width: '100%',
-    borderRadius: '8px'
+    padding: "10px 14px",
+    marginBottom: "4px",
+    outline: "none",
+    fontSize: "16px",
+    width: "100%",
+    borderRadius: "8px",
   },
   formContainer: {
-    display: 'flex',
-    flexDirection: 'column' as 'column',
-    margin: '20px auto 0px',
-    padding: '20px',
-    boxShadow: '0px 0px 12px rgba(198, 131, 255, .2)',
-    borderRadius: '10px'
+    display: "flex",
+    flexDirection: "column" as "column",
+    margin: "20px auto 0px",
+    padding: "20px",
+    boxShadow: "0px 0px 12px rgba(198, 131, 255, .2)",
+    borderRadius: "10px",
   },
   result: {
-    color: 'white'
-  }
-}
+    color: "white",
+  },
+};
--- pages/api/get-itinerary.ts
+++ pages/api/get-itinerary.ts
@@ -1,56 +1,52 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  message: string,
-  pointsOfInterestPrompt: any,
-  itinerary: any,
-}
+  message: string;
+  pointsOfInterestPrompt: any;
+  itinerary: any;
+};
 
-const GPT_KEY = process.env.GPT_API_KEY
+const GPT_KEY = process.env.GPT_API_KEY;
 
 const headers = {
-  'Content-Type': 'application/json',
-  'Authorization': `Bearer ${GPT_KEY}`
-}
+  "Content-Type": "application/json",
+  Authorization: `Bearer ${GPT_KEY}`,
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  let days = 4, city = 'Rio'
+  let city = "Rio";
   if (req.body) {
-    let body = JSON.parse(req.body)
-    days = body.days
-    city = body.city
+    let body = JSON.parse(req.body);
+    city = body.city;
   }
 
-  if (days > 10) {
-    days = 10
-  }
-
-  let basePrompt = `what is an ideal itinerary for ${days} days in ${city}?`
+  let basePrompt = `Give me the names of 10 popular bars where a group of friends may enjoy drinks in ${city}.`;
   try {
-    const response = await fetch('https://api.openai.com/v1/completions', {
-      method: 'POST',
+    const response = await fetch("https://api.openai.com/v1/completions", {
+      method: "POST",
       headers,
       body: JSON.stringify({
-        model: 'text-davinci-003',
+        model: "text-davinci-003",
         prompt: basePrompt,
         temperature: 0,
-        max_tokens: 550
-      })
-    })
-    const itinerary = await response.json()
-    const pointsOfInterestPrompt = 'Extract the points of interest out of this text, with no additional words, separated by commas: ' + itinerary.choices[0].text
+        max_tokens: 550,
+      }),
+    });
+    const itinerary = await response.json();
+    const pointsOfInterestPrompt =
+      "Extract the bar names out of this text, with no additional words, separated by commas: " +
+      itinerary.choices[0].text;
 
     res.status(200).json({
-      message: 'success',
+      message: "success",
       pointsOfInterestPrompt,
-      itinerary: itinerary.choices[0].text
-    })
-
+      itinerary: itinerary.choices[0].text,
+    });
   } catch (err) {
-    console.log('error: ', err)
+    console.log("error: ", err);
   }
 }
